96bb7f2b3be316635644b4a4fac259929bc5788c
Philipp <philipp.wundrack@live.de>
2023-12-20 17:49:54 +0100

    reduce boilerplate in tests

--- src/array/common.rs
+++ src/array/common.rs
@@ -10,56 +10,58 @@ pub struct Array<const D: usize> {
     pub(super) shape: [usize; D],
 }
 
-type Type1Function = fn(Array<1>, Array<1>) -> Array<1>;
-type Type2Function = fn(&mut Array<1>, Array<1>);
-type Type3Function = fn(&Array<1>, &Array<1>) -> Array<1>;
-type Type4Function = fn(&Array<1>) -> Array<1>;
-type Type5Function = fn(&Array<1>, &Array<1>, &Array<1>) -> Array<1>;
-type Type6Function = fn(&mut Array<1>, &Array<1>, &Array<1>);
-
 #[cfg(test)]
 mod tests {
     use std::ops::{Add, Sub, Mul, Div, AddAssign, SubAssign, MulAssign, DivAssign};
 
+    use rand::{distributions::{Distribution, Uniform}, SeedableRng};
+    use rand_chacha::ChaCha20Rng;
     use rstest::rstest;
 
     use super::*;
 
+    type Type1Function = fn(Array<1>, Array<1>) -> Array<1>;
+    type Type2Function = fn(&mut Array<1>, Array<1>);
+    type Type3Function = fn(&Array<1>, &Array<1>) -> Array<1>;
+    type Type4Function = fn(&Array<1>) -> Array<1>;
+    type Type5Function = fn(&Array<1>, &Array<1>, &Array<1>) -> Array<1>;
+    type Type6Function = fn(&mut Array<1>, &Array<1>, &Array<1>);
+
+    fn get_random_f32_vec(seed: u64, len: usize) -> Vec<f32> {
+        let mut rng = ChaCha20Rng::seed_from_u64(seed);
+        let distribution = Uniform::new(-10.0f32, 10.0f32);
+        let mut data = Vec::with_capacity(len);
+
+        for _ in 0..len {
+            data.push(distribution.sample(&mut rng));
+        }
+        
+        data
+    }
+
     #[test]
     fn conversion() {
-        let mut values = Vec::new();
-
         for i in 0..64 {
-            let converted: Array<1> = values.clone().into();
+            let data = get_random_f32_vec(0, i);
+            let converted: Array<1> = data.clone().into();
             let converted_back: Vec<f32> = converted.into();
 
-            assert_eq!(converted_back, values);
-
-            values.push(i as f32);
+            assert_eq!(converted_back, data);
         }
     }
 
     #[test]
     fn zeros() {
-        let mut values = Vec::new();
-
         for i in 0..64 {
             let zeros: Vec<f32> = Array::<1>::zeros(i).into();
 
-            assert_eq!(zeros, values);
-
-            values.push(0.0f32);
+            assert_eq!(zeros, vec![0.0; i]);
         }
     }
 
     #[test]
     fn get() {
-        let mut data = Vec::new();
-
-        for i in 0..64 {
-            data.push(i as f32);
-        }
-
+        let data = get_random_f32_vec(0, 64);
         let array: Array<1> = data.clone().into();
 
         for i in 0..64 {
@@ -70,24 +72,15 @@ mod tests {
     #[test]
     #[should_panic]
     fn get_out_of_bounds() {
-        let mut data = Vec::new();
+        let data = get_random_f32_vec(0, 64);
+        let array: Array<1> = data.into();
 
-        for i in 0..64 {
-            data.push(i as f32);
-        }
-
-        let array: Array<1> = data.clone().into();
         array.get(64);
     }
 
     #[test]
     fn set() {
-        let mut data = Vec::new();
-
-        for i in 0..64 {
-            data.push(i as f32);
-        }
-
+        let data = get_random_f32_vec(0, 64);
         let mut array: Array<1> = data.clone().into();
 
         for i in 0..64 {
@@ -99,13 +92,8 @@ mod tests {
     #[test]
     #[should_panic]
     fn set_out_of_bounds() {
-        let mut data = Vec::new();
-
-        for i in 0..64 {
-            data.push(i as f32);
-        }
-
-        let mut array: Array<1> = data.clone().into();
+        let data = get_random_f32_vec(0, 64);
+        let mut array: Array<1> = data.into();
         array.set(64, 42.0);
     }
 
@@ -115,10 +103,10 @@ mod tests {
     #[case::mul(Mul::mul, Mul::mul)]
     #[case::div(Div::div, Div::div)]
     fn type1(#[case] test_function: Type1Function, #[case] target_function: fn(f32, f32) -> f32) {
-        let mut data1 = Vec::new();
-        let mut data2 = Vec::new();
-
         for i in 0..64 {
+            let data1 = get_random_f32_vec(0, i);
+            let data2 = get_random_f32_vec(1, i);
+
             let array1: Array<1> = data1.clone().into();
             let array2: Array<1> = data2.clone().into();
 
@@ -127,9 +115,6 @@ mod tests {
             for ((d1, d2), r) in data1.iter().zip(data2.iter()).zip(result.iter()) {
                 assert_eq!(*r, target_function(*d1, *d2));
             }
-
-            data1.push(i as f32);
-            data2.push((i + 1) as f32);
         }
     }
 
@@ -140,8 +125,8 @@ mod tests {
     #[case::div(Div::div)]
     #[should_panic]
     fn type1_shape_mismatch(#[case] test_function: Type1Function) {
-        let array1: Array<1> = vec![0.0; 3].into();
-        let array2: Array<1> = vec![0.0; 4].into();
+        let array1: Array<1> = get_random_f32_vec(0, 3).into();
+        let array2: Array<1> = get_random_f32_vec(1, 4).into();
         let _ = test_function(array1, array2);
     }
 
@@ -151,10 +136,10 @@ mod tests {
     #[case::mul(MulAssign::mul_assign, Mul::mul)]
     #[case::div(DivAssign::div_assign, Div::div)]
     fn type2(#[case] test_function: Type2Function, #[case] target_function: fn(f32, f32) -> f32) {
-        let mut data1 = Vec::new();
-        let mut data2 = Vec::new();
-
         for i in 0..64 {
+            let data1 = get_random_f32_vec(0, i);
+            let data2 = get_random_f32_vec(1, i);
+
             let mut array1: Array<1> = data1.clone().into();
             let array2: Array<1> = data2.clone().into();
 
@@ -164,9 +149,6 @@ mod tests {
             for ((d1, d2), r) in data1.iter().zip(data2.iter()).zip(result.iter()) {
                 assert_eq!(*r, target_function(*d1, *d2));
             }
-
-            data1.push(i as f32); // TODO: use pseudo random numbers with a fixed seed
-            data2.push((i + 1) as f32);
         }
     }
 
@@ -177,8 +159,8 @@ mod tests {
     #[case::div(DivAssign::div_assign)]
     #[should_panic]
     fn type2_shape_mismatch(#[case] test_function: Type2Function) {
-        let mut array1: Array<1> = vec![0.0; 3].into();
-        let array2: Array<1> = vec![0.0; 4].into();
+        let mut array1: Array<1> = get_random_f32_vec(0, 3).into();
+        let array2: Array<1> = get_random_f32_vec(1, 4).into();
         let _ = test_function(&mut array1, array2);
     }
 
@@ -186,10 +168,10 @@ mod tests {
     #[case::max(Array::<1>::max, f32::max)]
     #[case::min(Array::<1>::min, f32::min)]
     fn type3(#[case] test_function: Type3Function, #[case] target_function: fn(f32, f32) -> f32) {
-        let mut data1 = Vec::new();
-        let mut data2 = Vec::new();
-
         for i in 0..64 {
+            let data1 = get_random_f32_vec(0, i);
+            let data2 = get_random_f32_vec(1, i);
+
             let array1: Array<1> = data1.clone().into();
             let array2: Array<1> = data2.clone().into();
 
@@ -198,9 +180,6 @@ mod tests {
             for ((d1, d2), r) in data1.iter().zip(data2.iter()).zip(result.iter()) {
                 assert_eq!(*r, target_function(*d1, *d2));
             }
-
-            data1.push(i as f32); // TODO: use pseudo random numbers with a fixed seed
-            data2.push((i + 1) as f32);
         }
     }
 
@@ -209,36 +188,33 @@ mod tests {
     #[case::min(Array::<1>::min)]
     #[should_panic]
     fn type3_shape_mismatch(#[case] test_function: Type3Function) {
-        let array1: Array<1> = vec![0.0; 3].into();
-        let array2: Array<1> = vec![0.0; 4].into();
+        let array1: Array<1> = get_random_f32_vec(0, 3).into();
+        let array2: Array<1> = get_random_f32_vec(1, 4).into();
         let _ = test_function(&array1, &array2);
     }
 
     #[rstest]
     #[case::sqrt(Array::<1>::sqrt)]
     fn type4(#[case] test_function: Type4Function) {
-        let mut data = Vec::new();
-
         for i in 0..64 {
+            let data = get_random_f32_vec(0, i);
             let array: Array<1> = data.clone().into();
             let result: Vec<f32> = test_function(&array).into();
 
             for (d, r) in data.iter().zip(result.iter()) {
                 assert_eq!(*r, d.sqrt());
             }
-
-            data.push(i as f32);
         }
     }
 
     #[rstest]
     #[case::fmadd(Array::<1>::fmadd, |x, y, z| y * z + x)]
     fn type5(#[case] test_function: Type5Function, #[case] target_function: fn(f32, f32, f32) -> f32) {
-        let mut data1 = Vec::new();
-        let mut data2 = Vec::new();
-        let mut data3 = Vec::new();
-
         for i in 0..64 {
+            let data1 = get_random_f32_vec(0, i);
+            let data2 = get_random_f32_vec(1, i);
+            let data3 = get_random_f32_vec(2, i);
+
             let array1: Array<1> = data1.clone().into();
             let array2: Array<1> = data2.clone().into();
             let array3: Array<1> = data3.clone().into();
@@ -248,10 +224,6 @@ mod tests {
             for (((d1, d2), d3), r) in data1.iter().zip(data2.iter()).zip(data3.iter()).zip(result.iter()) {
                 assert_eq!(*r, target_function(*d1, *d2, *d3));
             }
-
-            data1.push(i as f32);
-            data2.push((i + 1) as f32);
-            data3.push((i + 2) as f32);
         }
     }
 
@@ -259,9 +231,9 @@ mod tests {
     #[case::fmadd(Array::<1>::fmadd)]
     #[should_panic]
     fn type5_shape_mismatch(#[case] test_function: Type5Function) {
-        let a: Array<1> = vec![0.0; 3].into();
-        let b: Array<1> = vec![0.0; 4].into();
-        let c: Array<1> = vec![0.0; 5].into();
+        let a: Array<1> = get_random_f32_vec(0, 3).into();
+        let b: Array<1> = get_random_f32_vec(1, 4).into();
+        let c: Array<1> = get_random_f32_vec(2, 5).into();
 
         test_function(&a, &b, &c);
     }
@@ -269,11 +241,11 @@ mod tests {
     #[rstest]
     #[case::fmadd(Array::<1>::fmadd_in_place, |x, y, z| y * z + x)]
     fn fmadd_in_place(#[case] test_function: Type6Function, #[case] target_function: fn(f32, f32, f32) -> f32) {
-        let mut data1 = Vec::new();
-        let mut data2 = Vec::new();
-        let mut data3 = Vec::new();
-
         for i in 0..64 {
+            let data1 = get_random_f32_vec(0, i);
+            let data2 = get_random_f32_vec(1, i);
+            let data3 = get_random_f32_vec(2, i);
+
             let mut array1: Array<1> = data1.clone().into();
             let array2: Array<1> = data2.clone().into();
             let array3: Array<1> = data3.clone().into();
@@ -284,10 +256,6 @@ mod tests {
             for (((d1, d2), d3), r) in data1.iter().zip(data2.iter()).zip(data3.iter()).zip(result.iter()) {
                 assert_eq!(*r, target_function(*d1, *d2, *d3));
             }
-
-            data1.push(i as f32);
-            data2.push((i + 1) as f32);
-            data3.push((i + 2) as f32);
         }
     }
 
@@ -295,9 +263,9 @@ mod tests {
     #[case::fmadd(Array::<1>::fmadd_in_place)]
     #[should_panic]
     fn fmadd_in_place_shape_mismatch(#[case] test_function: Type6Function) {
-        let mut a: Array<1> = vec![0.0; 3].into();
-        let b: Array<1> = vec![0.0; 4].into();
-        let c: Array<1> = vec![0.0; 5].into();       
+        let mut a: Array<1> = get_random_f32_vec(0, 3).into();
+        let b: Array<1> = get_random_f32_vec(1, 4).into();
+        let c: Array<1> = get_random_f32_vec(2, 5).into();       
         
         test_function(&mut a, &b, &c);
     }
